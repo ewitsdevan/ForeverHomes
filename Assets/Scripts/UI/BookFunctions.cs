7be06a66ba650a5cb7aedb7c765ca7c3af62b1fc
Devan Laczko <1016781@student.sae.edu.au>
2024-09-26 15:03:52 +1000

    UI Elements

---
+++ Assets/Scripts/UI/BookFunctions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class BookFunctions : MonoBehaviour
+{
+    public void ToggleBook()
+    {
+        if (gameObject.activeSelf == false)
+        {
+            ShowBook();
+        }
+        else
+        {
+            HideBook();
+        }
+    }
+    
+    private void ShowBook()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void HideBook()
+    {
+        gameObject.SetActive(false);
+    }
+}
