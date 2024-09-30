c35abbe24876d9c6fe219883f031d84ef3492a59
Devan Laczko <1016781@student.sae.edu.au>
2024-09-30 12:24:17 +1000

    Quit Button

---
+++ Assets/Scripts/UI/UIQuitButton.cs
@@ -0,0 +1,14 @@
+// Created by Devan Laczko, 30/09/2024
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIQuitButton : MonoBehaviour
+{
+    // Quits Game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
