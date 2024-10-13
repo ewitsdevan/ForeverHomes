ca91875110a938ebf1bb9250d101719f0ba7c3a1
Mthynne <1027517@student.sae.edu.au>
2024-10-13 21:49:26 +1100

    Sewing Machine and Packages
    Added packages for ropes in game, Odin for optimization and easier use of stuff in unity + textmeshpro

---
+++ Assets/Scenes/Minigames/SewingCurtainScene/SewingGameManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class SewingGameManager : MonoBehaviour
+{
+    public Raycast raycast;
+    public NeedleControl needleControl;
+    
+    public bool hasFinished = false;
+    public bool hasWon = false;
+    public float winThreshold = 800f;
+    public float sewingScore = 0f;
+
+    public GameObject light1;
+    public GameObject light2;
+    public GameObject light3;
+
+    public Material green;
+    
+    public delegate void SewingGameStartEvent();
+    public event SewingGameStartEvent sewingGameStartEvent;
+    
+    public delegate void SewingGameEndEvent(bool hasWon);
+    public event SewingGameEndEvent sewingGameEndEvent;
+
+    [Button]
+    void BeginGame()
+    {
+        if (sewingGameStartEvent != null)
+        {
+            sewingGameStartEvent.Invoke();
+        }
+
+        StartCoroutine(waitToBegin());
+        
+    }
+
+    IEnumerator waitToBegin()
+    {
+        yield return new WaitForSeconds(1);
+        light1.GetComponent<Renderer>().material = green;
+        yield return new WaitForSeconds(1);
+        light2.GetComponent<Renderer>().material = green;
+        yield return new WaitForSeconds(1);
+        light3.GetComponent<Renderer>().material = green;
+        needleControl.enabled = true;
+        raycast.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (hasFinished)
+        {
+            //game has ended
+            if (sewingScore > winThreshold)
+            {
+                hasWon = true;
+                if (sewingGameEndEvent != null)
+                {
+                    sewingGameEndEvent.Invoke(hasWon);
+                }
+            }
+            else
+            {
+                hasWon = false;
+                if (sewingGameEndEvent != null)
+                {
+                    sewingGameEndEvent.Invoke(hasWon);
+                }
+            }
+        }
+        
+    }
+}
