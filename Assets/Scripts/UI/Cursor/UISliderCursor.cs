5271910bb2bea95f39a768739c120efe56e054cf
Devan Laczko <1016781@student.sae.edu.au>
2024-12-08 13:15:15 +1100

    Cursor

---
+++ Assets/Scripts/UI/Cursor/UISliderCursor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UISliderCursor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = new Vector2(10, 10);
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (gameObject.GetComponent<Slider>().interactable)
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Pass 'null' to the texture parameter to use the default system cursor.
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
